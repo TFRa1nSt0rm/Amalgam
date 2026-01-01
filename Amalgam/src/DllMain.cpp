c948808c074ea705eb844c714ba8a95bc1dca8ae
TFRa1nSt0rm <163467423+TFRa1nSt0rm@users.noreply.github.com>
2026-01-01 18:58:44 -0500

    Universal Injection Support
    All methods of injection with GuidedHacking Injector supported
    Screw you, ManualMap is always better

--- Amalgam/src/DllMain.cpp
+++ Amalgam/src/DllMain.cpp
@@ -2,26 +2,56 @@
 #include "Core/Core.h"
 #include "Utils/ExceptionHandler/ExceptionHandler.h"
 
+// Track if we were manually mapped
+static bool g_bManualMap = false;
+static HMODULE g_hModule = nullptr;
+
 DWORD WINAPI MainThread(LPVOID lpParam)
 {
-	U::ExceptionHandler.Initialize(lpParam);
-
-	U::Core.Load();
-	U::Core.Loop();
-	U::Core.Unload();
-
-	U::ExceptionHandler.Unload();
-
-	FreeLibraryAndExitThread(static_cast<HMODULE>(lpParam), EXIT_SUCCESS);
+    U::ExceptionHandler.Initialize(lpParam);
+    U::Core.Load();
+    U::Core.Loop();
+    U::ExceptionHandler.Unload();
+    U::Core.Unload();
+    
+    // Only call FreeLibraryAndExitThread if we were loaded normally
+    if (!g_bManualMap)
+    {
+        FreeLibraryAndExitThread(static_cast<HMODULE>(lpParam), EXIT_SUCCESS);
+    }
+    
+    return EXIT_SUCCESS;
 }
 
+// Manual map entry point
+extern "C" __declspec(dllexport) BOOL WINAPI ManualMapEntry(HMODULE hModule)
+{
+    g_bManualMap = true;
+    g_hModule = hModule;
+    
+    if (const auto hThread = CreateThread(nullptr, 0, MainThread, hModule, 0, nullptr))
+    {
+        CloseHandle(hThread);
+        return TRUE;
+    }
+    
+    return FALSE;
+}
+
+// Standard injection entry point (works for all LoadLibrary variants and Ldr* functions)
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
-	if (fdwReason == DLL_PROCESS_ATTACH)
-	{
-		if (const auto hThread = CreateThread(nullptr, 0, MainThread, hinstDLL, 0, nullptr))
-			CloseHandle(hThread);
-	}
-
-	return TRUE;
+    if (fdwReason == DLL_PROCESS_ATTACH)
+    {
+        DisableThreadLibraryCalls(hinstDLL);
+        g_hModule = hinstDLL;
+        g_bManualMap = false;
+        
+        if (const auto hThread = CreateThread(nullptr, 0, MainThread, hinstDLL, 0, nullptr))
+        {
+            CloseHandle(hThread);
+        }
+    }
+    
+    return TRUE;
 }
